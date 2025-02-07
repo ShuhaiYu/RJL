3bda3b3882880a699c4e3ed8ae2bee9e059a6618
EdgarTang <tang@veiam.net>
2025-02-07 18:16:35 +1100

    style: hide unused side menu

--- src/config/menu.config.jsx
+++ src/config/menu.config.jsx
@@ -1,18 +1,18 @@
 export const MENU_SIDEBAR = [
-  {
-    title: "Dashboards",
-    icon: "element-11",
-    children: [
-      {
-        title: "Light Sidebar",
-        path: "/",
-      },
-      {
-        title: "Dark Sidebar",
-        path: "/dark-sidebar",
-      },
-    ],
-  },
+  // {
+  //   title: "Dashboards",
+  //   icon: "element-11",
+  //   children: [
+  //     {
+  //       title: "Light Sidebar",
+  //       path: "/",
+  //     },
+  //     {
+  //       title: "Dark Sidebar",
+  //       path: "/dark-sidebar",
+  //     },
+  //   ],
+  // },
   {
     heading: "User",
   },
@@ -73,7 +73,7 @@ export const MENU_SIDEBAR = [
       // },
     ],
   },
-  
+
   {
     title: "Agencies",
     icon: "users",
@@ -86,7 +86,7 @@ export const MENU_SIDEBAR = [
         title: "Create Agency",
         path: "/agencies/create-agency",
       },
-    ],  
+    ],
   },
   {
     title: "Properties",
@@ -100,181 +100,181 @@ export const MENU_SIDEBAR = [
         title: "Tasks",
         path: "/property/tasks",
       },
-    ],  
-  },
-  {
-    title: "My Account",
-    icon: "setting-2",
-    children: [
-      {
-        title: "Account",
-        children: [
-          // {
-          //   title: "Get Started",
-          //   path: "/account/home/get-started",
-          // },
-          {
-            title: "User Profile",
-            path: "/account/home/user-profile",
-          },
-          {
-            title: "Company Profile",
-            path: "/account/home/company-profile",
-          },
-          // {
-          //   title: "Settings - With Sidebar",
-          //   path: "/account/home/settings-sidebar",
-          // },
-          // {
-          //   title: "Settings - Enterprise",
-          //   path: "/account/home/settings-enterprise",
-          // },
-          // {
-          //   title: "Settings - Plain",
-          //   path: "/account/home/settings-plain",
-          // },
-          // {
-          //   title: "Settings - Modal",
-          //   path: "/account/home/settings-modal",
-          // },
-        ],
-      },
-      // {
-      //   title: "Billing",
-      //   children: [
-      //     {
-      //       title: "Billing - Basic",
-      //       path: "/account/billing/basic",
-      //     },
-      //     {
-      //       title: "Billing - Enterprise",
-      //       path: "/account/billing/enterprise",
-      //     },
-      //     {
-      //       title: "Plans",
-      //       path: "/account/billing/plans",
-      //     },
-      //     {
-      //       title: "Billing History",
-      //       path: "/account/billing/history",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Security",
-      //   children: [
-      //     {
-      //       title: "Get Started",
-      //       path: "/account/security/get-started",
-      //     },
-      //     {
-      //       title: "Security Overview",
-      //       path: "/account/security/overview",
-      //     },
-      //     {
-      //       title: "Allowed IP Addresses",
-      //       path: "/account/security/allowed-ip-addresses",
-      //     },
-      //     {
-      //       title: "Privacy Settings",
-      //       path: "/account/security/privacy-settings",
-      //     },
-      //     {
-      //       title: "Device Management",
-      //       path: "/account/security/device-management",
-      //     },
-      //     {
-      //       title: "Backup & Recovery",
-      //       path: "/account/security/backup-and-recovery",
-      //     },
-      //     {
-      //       title: "Current Sessions",
-      //       path: "/account/security/current-sessions",
-      //     },
-      //     {
-      //       title: "Security Log",
-      //       path: "/account/security/security-log",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Members & Roles",
-      //   children: [
-      //     {
-      //       title: "Teams Starter",
-      //       path: "/account/members/team-starter",
-      //     },
-      //     {
-      //       title: "Teams",
-      //       path: "/account/members/teams",
-      //     },
-      //     {
-      //       title: "Team Info",
-      //       path: "/account/members/team-info",
-      //     },
-      //     {
-      //       title: "Members Starter",
-      //       path: "/account/members/members-starter",
-      //     },
-      //     {
-      //       title: "Team Members",
-      //       path: "/account/members/team-members",
-      //     },
-      //     {
-      //       title: "Import Members",
-      //       path: "/account/members/import-members",
-      //     },
-      //     {
-      //       title: "Roles",
-      //       path: "/account/members/roles",
-      //     },
-      //     {
-      //       title: "Permissions - Toggler",
-      //       path: "/account/members/permissions-toggle",
-      //     },
-      //     {
-      //       title: "Permissions - Check",
-      //       path: "/account/members/permissions-check",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Integrations",
-      //   path: "/account/integrations",
-      // },
-      // {
-      //   title: "Notifications",
-      //   path: "/account/notifications",
-      // },
-      // {
-      //   title: "API Keys",
-      //   path: "/account/api-keys",
-      // },
-      // {
-      //   title: "More",
-      //   collapse: true,
-      //   collapseTitle: "Show less",
-      //   expandTitle: "Show 3 more",
-      //   dropdownProps: {
-      //     placement: "right-start",
-      //   },
-      //   children: [
-      //     {
-      //       title: "Appearance",
-      //       path: "/account/appearance",
-      //     },
-      //     {
-      //       title: "Invite a Friend",
-      //       path: "/account/invite-a-friend",
-      //     },
-      //     {
-      //       title: "Activity",
-      //       path: "/account/activity",
-      //     },
-      //   ],
-      // },
     ],
   },
+  // {
+  //   title: "My Account",
+  //   icon: "setting-2",
+  //   children: [
+  //     {
+  //       title: "Account",
+  //       children: [
+  //         // {
+  //         //   title: "Get Started",
+  //         //   path: "/account/home/get-started",
+  //         // },
+  //         {
+  //           title: "User Profile",
+  //           path: "/account/home/user-profile",
+  //         },
+  //         {
+  //           title: "Company Profile",
+  //           path: "/account/home/company-profile",
+  //         },
+  //         // {
+  //         //   title: "Settings - With Sidebar",
+  //         //   path: "/account/home/settings-sidebar",
+  //         // },
+  //         // {
+  //         //   title: "Settings - Enterprise",
+  //         //   path: "/account/home/settings-enterprise",
+  //         // },
+  //         // {
+  //         //   title: "Settings - Plain",
+  //         //   path: "/account/home/settings-plain",
+  //         // },
+  //         // {
+  //         //   title: "Settings - Modal",
+  //         //   path: "/account/home/settings-modal",
+  //         // },
+  //       ],
+  //     },
+  //     // {
+  //     //   title: "Billing",
+  //     //   children: [
+  //     //     {
+  //     //       title: "Billing - Basic",
+  //     //       path: "/account/billing/basic",
+  //     //     },
+  //     //     {
+  //     //       title: "Billing - Enterprise",
+  //     //       path: "/account/billing/enterprise",
+  //     //     },
+  //     //     {
+  //     //       title: "Plans",
+  //     //       path: "/account/billing/plans",
+  //     //     },
+  //     //     {
+  //     //       title: "Billing History",
+  //     //       path: "/account/billing/history",
+  //     //     },
+  //     //   ],
+  //     // },
+  //     // {
+  //     //   title: "Security",
+  //     //   children: [
+  //     //     {
+  //     //       title: "Get Started",
+  //     //       path: "/account/security/get-started",
+  //     //     },
+  //     //     {
+  //     //       title: "Security Overview",
+  //     //       path: "/account/security/overview",
+  //     //     },
+  //     //     {
+  //     //       title: "Allowed IP Addresses",
+  //     //       path: "/account/security/allowed-ip-addresses",
+  //     //     },
+  //     //     {
+  //     //       title: "Privacy Settings",
+  //     //       path: "/account/security/privacy-settings",
+  //     //     },
+  //     //     {
+  //     //       title: "Device Management",
+  //     //       path: "/account/security/device-management",
+  //     //     },
+  //     //     {
+  //     //       title: "Backup & Recovery",
+  //     //       path: "/account/security/backup-and-recovery",
+  //     //     },
+  //     //     {
+  //     //       title: "Current Sessions",
+  //     //       path: "/account/security/current-sessions",
+  //     //     },
+  //     //     {
+  //     //       title: "Security Log",
+  //     //       path: "/account/security/security-log",
+  //     //     },
+  //     //   ],
+  //     // },
+  //     // {
+  //     //   title: "Members & Roles",
+  //     //   children: [
+  //     //     {
+  //     //       title: "Teams Starter",
+  //     //       path: "/account/members/team-starter",
+  //     //     },
+  //     //     {
+  //     //       title: "Teams",
+  //     //       path: "/account/members/teams",
+  //     //     },
+  //     //     {
+  //     //       title: "Team Info",
+  //     //       path: "/account/members/team-info",
+  //     //     },
+  //     //     {
+  //     //       title: "Members Starter",
+  //     //       path: "/account/members/members-starter",
+  //     //     },
+  //     //     {
+  //     //       title: "Team Members",
+  //     //       path: "/account/members/team-members",
+  //     //     },
+  //     //     {
+  //     //       title: "Import Members",
+  //     //       path: "/account/members/import-members",
+  //     //     },
+  //     //     {
+  //     //       title: "Roles",
+  //     //       path: "/account/members/roles",
+  //     //     },
+  //     //     {
+  //     //       title: "Permissions - Toggler",
+  //     //       path: "/account/members/permissions-toggle",
+  //     //     },
+  //     //     {
+  //     //       title: "Permissions - Check",
+  //     //       path: "/account/members/permissions-check",
+  //     //     },
+  //     //   ],
+  //     // },
+  //     // {
+  //     //   title: "Integrations",
+  //     //   path: "/account/integrations",
+  //     // },
+  //     // {
+  //     //   title: "Notifications",
+  //     //   path: "/account/notifications",
+  //     // },
+  //     // {
+  //     //   title: "API Keys",
+  //     //   path: "/account/api-keys",
+  //     // },
+  //     // {
+  //     //   title: "More",
+  //     //   collapse: true,
+  //     //   collapseTitle: "Show less",
+  //     //   expandTitle: "Show 3 more",
+  //     //   dropdownProps: {
+  //     //     placement: "right-start",
+  //     //   },
+  //     //   children: [
+  //     //     {
+  //     //       title: "Appearance",
+  //     //       path: "/account/appearance",
+  //     //     },
+  //     //     {
+  //     //       title: "Invite a Friend",
+  //     //       path: "/account/invite-a-friend",
+  //     //     },
+  //     //     {
+  //     //       title: "Activity",
+  //     //       path: "/account/activity",
+  //     //     },
+  //     //   ],
+  //     // },
+  //   ],
+  // },
   // {
   //   title: "Network",
   //   icon: "users",
