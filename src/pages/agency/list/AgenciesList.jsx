fd65dd747e4573ae36a66c9453d686ff65843a14
EdgarTang <tang@veiam.net>
2025-02-07 18:49:27 +1100

    style: hide agency list buttons

--- src/pages/agency/list/AgenciesList.jsx
+++ src/pages/agency/list/AgenciesList.jsx
@@ -9,30 +9,30 @@ import { Agencies } from './blocks';
 const AgenciesList = () => {
   const image = <img src={toAbsoluteUrl('/media/avatars/300-1.png')} className="rounded-full border-3 border-success h-[100px] shrink-0" />;
   return <Fragment>
-      <UserProfileHero name="Jenny Klabber" image={image} info={[{
-      label: 'KeenThemes',
-      icon: 'abstract'
-    }, {
-      label: 'SF, Bay Area',
-      icon: 'geolocation'
-    }, {
-      email: 'jenny@kteam.com',
-      icon: 'sms'
-    }]} />
+    {/*  <UserProfileHero name="Jenny Klabber" image={image} info={[{*/}
+    {/*  label: 'KeenThemes',*/}
+    {/*  icon: 'abstract'*/}
+    {/*}, {*/}
+    {/*  label: 'SF, Bay Area',*/}
+    {/*  icon: 'geolocation'*/}
+    {/*}, {*/}
+    {/*  email: 'jenny@kteam.com',*/}
+    {/*  icon: 'sms'*/}
+    {/*}]} />*/}
 
       <Container>
         <Navbar>
           {/* <PageMenu /> */}
 
-          <NavbarActions>
-            <button type="button" className="btn btn-sm btn-primary">
-              <KeenIcon icon="users" /> Connect
-            </button>
-            <button className="btn btn-sm btn-icon btn-light">
-              <KeenIcon icon="messages" />
-            </button>
-            <NavbarDropdown />
-          </NavbarActions>
+          {/*<NavbarActions>*/}
+          {/*  <button type="button" className="btn btn-sm btn-primary">*/}
+          {/*    <KeenIcon icon="users" /> Connect*/}
+          {/*  </button>*/}
+          {/*  <button className="btn btn-sm btn-icon btn-light">*/}
+          {/*    <KeenIcon icon="messages" />*/}
+          {/*  </button>*/}
+          {/*  <NavbarDropdown />*/}
+          {/*</NavbarActions>*/}
         </Navbar>
       </Container>
 
