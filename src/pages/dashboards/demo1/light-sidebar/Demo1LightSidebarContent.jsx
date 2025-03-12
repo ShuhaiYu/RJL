bbd65615237b67e94bb8b675d82a3ca301a4dfc3
EdgarTang <tang@veiam.net>
2025-03-12 18:12:28 +1100

    style:hide dashboard button

--- src/pages/dashboards/demo1/light-sidebar/Demo1LightSidebarContent.jsx
+++ src/pages/dashboards/demo1/light-sidebar/Demo1LightSidebarContent.jsx
@@ -6,12 +6,12 @@ const Demo1LightSidebarContent = () => {
   return (
     <div className="grid gap-5 lg:gap-7.5">
       <div className="w-full grid lg:grid-cols-3 gap-5 lg:gap-7.5 items-stretch">
-        <div className="lg:col-span-1">
-          <SendReminderButton />
-        </div>
-        <div className="lg:col-span-1">
-          <SyncLastWeekEmails />
-        </div>
+        {/*<div className="lg:col-span-1">*/}
+        {/*  <SendReminderButton />*/}
+        {/*</div>*/}
+        {/*<div className="lg:col-span-1">*/}
+        {/*  <SyncLastWeekEmails />*/}
+        {/*</div>*/}
       </div>
 
       <div className="grid lg:grid-cols-1 gap-5 lg:gap-7.5 items-stretch">
